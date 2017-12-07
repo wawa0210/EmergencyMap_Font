3729b308894c2d920f12690aabfa133f246fd4da
wawa0210 <xiaozhang0210@hotmail.com>
2017-12-07 11:18:49 +0800

    INIT

---
+++ js/common.js
@@ -0,0 +1,60 @@
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '' + s4() + '' + s4() + '' +
+        s4() + '' + s4() + s4() + s4();
+}
+
+//依赖全局WebApiHost参数
+var WebApiToken;
+var WebApiHost="http://localhost:4024/";
+function doRequest(apiHost, method, data, callback, contentType, showtips) {
+    //GetCookie
+    if (!WebApiToken) {
+        var cookies = document.cookie.split('; ');
+        for (var i = 0, parts; (parts = cookies[i] && cookies[i].split('=')); i++) {
+            if (parts.shift() === 'token') {
+                WebApiToken = parts.join('=');
+            }
+        }
+    }
+
+    var requestType = 'application/x-www-form-urlencoded';
+    if (contentType && contentType === 'json') {
+        requestType = 'application/json';
+    }
+
+    $.ajax({
+        type: method,
+        url: apiHost,
+        headers: {
+            token: WebApiToken,
+            "request-id": guid() + new Date().getTime()
+        },
+        data: data,
+        contentType: requestType,
+        //xhrFields: {
+        //    withCredentials: true
+        //},
+        //crossDomain: true,
+        success: function (data) {
+            callback(data);
+        },
+        error: function (err) {
+            //$.dialog.tips("Request Error!");
+        }
+    });
+}
+
+function doGet(baseUrl, callback, showtips) {
+    var requestUri = WebApiHost + "v0" + baseUrl;
+    doRequest(requestUri, "GET", null, callback);
+}
+
+function doPost(baseUrl, data, callback, contentType, showtips) {
+    var requestUri = WebApiHost + "v0" + baseUrl;
+    doRequest(requestUri, "POST", data, callback, contentType);
+}
