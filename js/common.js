30b6b453eabd5ccf2f52910f8b86b4c983ba8c36
wawa0210 <xiaozhang0210@hotmail.com>
2017-12-09 15:59:33 +0800

    更新企业信息ok

--- js/common.js
+++ js/common.js
@@ -58,3 +58,15 @@ function doPost(baseUrl, data, callback, contentType, showtips) {
     var requestUri = WebApiHost + "v0" + baseUrl;
     doRequest(requestUri, "POST", data, callback, contentType);
 }
+
+function doPut(baseUrl, data, callback, contentType, showtips) {
+    var requestUri = WebApiHost + "v0" + baseUrl;
+    doRequest(requestUri, "PUT", data, callback, contentType);
+}
+
+//获取url中的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
