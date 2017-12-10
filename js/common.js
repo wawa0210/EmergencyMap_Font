c03fefefadb1211b1eddd2348434a020ff2061dd
wawa0210 <xiaozhang0210@hotmail.com>
2017-12-10 13:13:09 +0800

    edit:增加地图搜索添加地理位置信息

--- js/common.js
+++ js/common.js
@@ -68,5 +68,5 @@ function doPut(baseUrl, data, callback, contentType, showtips) {
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-    if (r != null) return unescape(r[2]); return null; //返回参数值
+    if (r != null) return decodeURI(r[2]); return null; //返回参数值
 }
